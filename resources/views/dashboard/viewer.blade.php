d5a3dd7a8f91c717eb27e6af53c3e201852d9fc0
bowoxdaily <ikhwanudinaldi04@gmail.com>
2026-07-21 11:13:52 +0700

    update viewer

--- resources/views/dashboard/viewer.blade.php
+++ resources/views/dashboard/viewer.blade.php
@@ -4,7 +4,14 @@
 
 @section('content')
     <div class="row">
-        <!-- Welcome Card -->
+        <!-- Statistik Hari Ini -->
+                <div class="col-lg-6 col-md-12 col-6 mb-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="card-title d-flex align-items-start justify-content-between">
+                                <div class="avatar flex-shrink-0">
+                                    <i class="bx bx-user-check bx-md text-success"></i>
+                                </div>
                             </div>
                             <span class="fw-semibold d-block mb-1">Hadir Hari Ini</span>
                             <h3 class="card-title mb-2">{{ $hadirHariIni }}</h3>
